1a0e4057f02faec579345d37d9bff9ce7bccd1de
Jim Kirkbride <james.kirkbride@gmail.com>
2017-02-11 06:38:40 -0500

    Refactoring.

--- src/main/java/com/jameskbride/tetris/Board.java
+++ src/main/java/com/jameskbride/tetris/Board.java
@@ -2,26 +2,31 @@ package com.jameskbride.tetris;
 
 public class Board {
 
-    public static final int BOARD_HEIGHT = 24;
-    public static final int BOARD_WIDTH = 10;
+    private static final int BOARD_HEIGHT = 24;
+    private static final int BOARD_WIDTH = 10;
+    private static final String EMPTY_SPACE = " ";
+    
     String[][] boardData;
 
     public Board() {
+        initializeBoard();
+    }
+
+    private void initializeBoard() {
         boardData = new String[BOARD_HEIGHT][BOARD_WIDTH];
         for (int rowIndex=0; rowIndex<BOARD_HEIGHT; rowIndex++) {
             for (int columnIndex=0; columnIndex<BOARD_WIDTH; columnIndex++) {
-                boardData[rowIndex][columnIndex] = " ";
+                boardData[rowIndex][columnIndex] = EMPTY_SPACE;
             }
         }
     }
 
     public int getHeight() {
-        return 24;
+        return BOARD_HEIGHT;
     }
 
-
     public int getWidth() {
-        return 10;
+        return BOARD_WIDTH;
     }
 
     public String getLocation(int rowIndex, int columnIndex) {
